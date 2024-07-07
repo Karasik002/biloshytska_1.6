a30a097c5a4d405566c959b79b93d67f803e501e
Karasik002 <kn23-m.biloshytska@nubip.edu.ua>
2024-07-07 23:05:05 +0300

    Add files via upload

---
+++ ПРАКТИКА 1.6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <windows.h>
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int x1, y1, x2, y2;
+    printf("Введіть координати x1, y1, x2, y2 через пробіл: ");
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+    double length = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+    printf("Довжина вектора: %.6f\n", length);
+
+    return 0;
+}
